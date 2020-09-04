2a05f17464cdacb775fe837247a5d835820ad95f
David Heyman <dave@axismaps.com>
2020-09-04 13:03:06 +0100

    Check if dir is a directory

--- resize_images.py
+++ resize_images.py
@@ -10,21 +10,22 @@ SIZES = {
 }
 
 for dir in os.listdir('images'):
-  for file in os.listdir(os.path.join('images', dir)):
-    if re.search(r"\.(tif|png|jpe?g)", file, re.IGNORECASE):
-      path = os.path.join("output/", re.sub(r"\.(tif|png|jpe?g)", "", file, 0, re.IGNORECASE))
-      Path(path).mkdir(parents=True, exist_ok=True)
-      try: 
-        img = Image.open(os.path.join('images', dir, file))
-        for key in SIZES:
-          maxDim = img.size.index(min(img.size))
-          minDim = abs(maxDim - 1)
-          ratio = SIZES[key] / img.size[maxDim]
-          if ratio < 1:
-            size = (round(img.size[0] * ratio), round(img.size[1] * ratio))
-            resized = img.resize(size, Image.ANTIALIAS)
-          else:
-            resized = img
-          resized.save(os.path.join(path, key + '.jpg'))
-      except:
-        print('Could not open ' + file)
+  if os.path.isdir(os.path.join('images', dir)):
+    for file in os.listdir(os.path.join('images', dir)):
+      if re.search(r"\.(tif|png|jpe?g)", file, re.IGNORECASE):
+        path = os.path.join("output/", re.sub(r"\.(tif|png|jpe?g)", "", file, 0, re.IGNORECASE))
+        Path(path).mkdir(parents=True, exist_ok=True)
+        try: 
+          img = Image.open(os.path.join('images', dir, file))
+          for key in SIZES:
+            maxDim = img.size.index(min(img.size))
+            minDim = abs(maxDim - 1)
+            ratio = SIZES[key] / img.size[maxDim]
+            if ratio < 1:
+              size = (round(img.size[0] * ratio), round(img.size[1] * ratio))
+              resized = img.resize(size, Image.ANTIALIAS)
+            else:
+              resized = img
+            resized.save(os.path.join(path, key + '.jpg'))
+        except:
+          print('Could not open ' + os.path.join(dir, file))
