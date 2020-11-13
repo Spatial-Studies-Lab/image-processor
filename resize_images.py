b61678834e01a64dab113d547cca2cd2aa5cb898
David Heyman <davidheyman@BAIT.cable.virginm.net>
2020-11-13 14:08:46 +0000

    Use source / target buckets

--- resize_images.py
+++ resize_images.py
@@ -14,7 +14,7 @@ SIZES = {
 
 s3 = boto3.client('s3')
 
-images = s3.list_objects(Bucket=os.environ['AWS_BUCKET'])
+images = s3.list_objects(Bucket=os.environ['BUCKET_SOURCE'])
 
 for result in images['Contents']:
   key = result['Key']
@@ -24,7 +24,7 @@ for result in images['Contents']:
     path = os.path.join("output/", ssid)
     with open(file, 'wb') as i:
       print('Downloading', file)
-      s3.download_fileobj(os.environ['AWS_BUCKET'], key, i)
+      s3.download_fileobj(os.environ['BUCKET_SOURCE'], key, i)
       try: 
         img = Image.open(file)
         print('Resizing', file)
@@ -40,7 +40,7 @@ for result in images['Contents']:
           filename = os.path.join("output/", kind + '.jpg')
           resized.save(filename)
           print('Uploading', filename)
-          s3.upload_file(filename, os.environ['AWS_BUCKET'], re.sub(r"output", ssid, filename))
+          s3.upload_file(filename, os.environ['BUCKET_TARGET'], re.sub(r"output", ssid, filename))
           os.remove(filename)
       except:
         print('Could not open ' + os.path.join(dir, file))
